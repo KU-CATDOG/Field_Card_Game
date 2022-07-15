b178996b8941e94673737dde32d6f7e120159e8b
Ji-Soo-Lee <jessie428@korea.ac.kr>
2022-07-15 15:12:49 +0900

    보호 카드 및 버프 구현

---
+++ FieldCardGame/Assets/Scripts/Cards/PaladinProtect.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaladinProtect : IPlayerCard
+{
+    private int range = 0;
+    private int amount = 20;
+    private bool interrupted;
+    public int GetRange()
+    {
+        return range;
+    }
+    public void SetRange(int _range)
+    {
+        range = _range;
+    }
+    public int GetAmount()
+    {
+        return amount;
+    }
+    public void SetAmount(int _amount)
+    {
+        amount = _amount;
+    }
+    public Color GetUnAvailableTileColor()
+    {
+        return Color.red;
+    }
+    public List<Coordinate> GetAvailableTile(Coordinate pos)
+    {
+        List<Coordinate> ret = new List<Coordinate>();
+        ret.Add(pos);
+        return ret;
+    }
+    public Color GetAvailableTileColor()
+    {
+        return Color.blue;
+    }
+    public List<Coordinate> GetAreaofEffect()
+    {
+        List<Coordinate> ret = new List<Coordinate>();
+        ret.Add(new Coordinate(0, 0));
+        return ret;
+    }
+    public Color GetColorOfEffect(Coordinate pos)
+    {
+        if (pos.X == 0 && pos.Y == 0)
+        {
+            return Color.white;
+        }
+        return Color.black;
+    }
+    public bool IsAvailablePosition(Coordinate caster, Coordinate target)
+    {
+        List<Coordinate> availablePositions = GetAvailableTile(caster);
+        if (availablePositions.Exists((i) => i.X == target.X && i.Y == target.Y))
+        {
+            return true;
+        }
+        return false;
+    }
+    public IEnumerator CardRoutine(Character caster, Coordinate target)
+    {
+        if (interrupted)
+        {
+             interrupted = false;
+             yield break;
+        }
+        caster.BuffHandler.Protect(GetAmount());
+        yield break;
+    }
+    public void CardRoutineInterrupt()
+    {
+        interrupted = true;
+    }
+    public int GetCost()
+    {
+        return 2;
+    }
+    public CostType GetCostType()
+    {
+        return CostType.PaladinEnergy;
+    }
+    public CardType GetCardType()
+    {
+        return CardType.Skill;
+    }
+    public int GetCardID()
+    {
+        return 1104001;
+    }
+}
