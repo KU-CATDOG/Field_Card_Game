21296d3b7e0736871f59dd8f6ca637f74383cfc8
Hena <hena0914@korea.ac.kr>
2022-07-15 20:03:42 +0900

    성기사 빛내림 카드 구현

---
+++ FieldCardGame/Assets/Scripts/Cards/PaladinShining.cs
@@ -0,0 +1,169 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaladinShining : IPlayerCard
+{
+    private int range = 3;
+    private int damage = 25;
+    private bool interrupted;
+    public int GetRange()
+    {
+        return range;
+    }
+    public void SetRange(int _range)
+    {
+        range = _range;
+    }
+    public int GetDamage()
+    {
+        return damage;
+    }
+    public void SetDamage(int _damage)
+    {
+        damage = _damage;
+    }
+    public Color GetUnAvailableTileColor()
+    {
+        return Color.red;
+    }
+    public List<Coordinate> GetAvailableTile(Coordinate pos)
+    {
+        List<Coordinate> ret = new List<Coordinate>();
+        int level = 1;
+        bool[,] visited = new bool[128, 128];
+        Queue<Coordinate> queue = new Queue<Coordinate>();
+        Queue<Coordinate> nextQueue = new Queue<Coordinate>();
+        queue.Enqueue(pos);
+        while (level++ <= GetRange())
+        {
+            while (queue.Count != 0)
+            {
+                Coordinate tmp = queue.Dequeue();
+                if (tmp.X != pos.X || tmp.Y != pos.Y)
+                    ret.Add(tmp);
+                Coordinate tile;
+                if ((tile = tmp.GetDownTile()) != null && !visited[tile.X, tile.Y] && !GameManager.Instance.Map[tile.X, tile.Y].CharacterOnTile)
+                {
+                    visited[tile.X, tile.Y] = true;
+                    nextQueue.Enqueue(tile);
+                };
+                if ((tile = tmp.GetLeftTile()) != null && !visited[tile.X, tile.Y] && !GameManager.Instance.Map[tile.X, tile.Y].CharacterOnTile)
+                {
+                    visited[tile.X, tile.Y] = true;
+                    nextQueue.Enqueue(tile);
+                };
+                if ((tile = tmp.GetRightTile()) != null && !visited[tile.X, tile.Y] && !GameManager.Instance.Map[tile.X, tile.Y].CharacterOnTile)
+                {
+                    visited[tile.X, tile.Y] = true;
+                    nextQueue.Enqueue(tile);
+                };
+                if ((tile = tmp.GetUpTile()) != null && !visited[tile.X, tile.Y] && !GameManager.Instance.Map[tile.X, tile.Y].CharacterOnTile)
+                {
+                    visited[tile.X, tile.Y] = true;
+                    nextQueue.Enqueue(tile);
+                }
+            }
+            queue = new Queue<Coordinate>(nextQueue);
+            nextQueue.Clear();
+        }
+        while (queue.Count != 0)
+        {
+            ret.Add(queue.Dequeue());
+        }
+        return ret;
+    }
+    public Color GetAvailableTileColor()
+    {
+        return Color.blue;
+    }
+    public List<Coordinate> GetAreaofEffect()
+    {
+        List<Coordinate> ret = new List<Coordinate>();
+        Coordinate pos = new Coordinate(0, 0);
+        Coordinate tile;
+        tile = pos.GetDownTilewithoutTest();
+        ret.Add(tile);
+        tile = pos.GetLeftTilewithoutTest();
+        ret.Add(tile);
+        tile = pos.GetRightTilewithoutTest();
+        ret.Add(tile);
+        tile = pos.GetUpTilewithoutTest();
+        ret.Add(tile);
+        return ret;
+    }
+    public Color GetColorOfEffect(Coordinate pos)
+    {
+        if (pos.X == 0 && pos.Y == 0)
+        {
+            return Color.white;
+        }
+        return Color.red;
+    }
+    public bool IsAvailablePosition(Coordinate caster, Coordinate target)
+    {
+        List<Coordinate> availablePositions = GetAvailableTile(caster);
+        if (availablePositions.Exists((i) => i.X == target.X && i.Y == target.Y))
+        {
+            return true;
+        }
+        return false;
+    }
+    public IEnumerator CardRoutine(Character caster, Coordinate target)
+    {
+        List<Coordinate> attack;
+        List<Coordinate> available = new List<Coordinate>();
+        Character tmp;
+        Coordinate pos;
+        attack = GetAreaofEffect();
+        for (int i = 0; i<attack.Count;i++)
+        {
+            pos = attack[i] + target;
+            if (Coordinate.OutRange(pos))
+            {
+                continue;
+            }
+            if (interrupted)
+            {
+                interrupted = false;
+                yield break;
+            }
+            tmp = GameManager.Instance.Map[pos.X, pos.Y].CharacterOnTile;
+            if (tmp)
+            {
+                available.Add(pos);
+            }
+        }
+        if (available.Count == 0)
+            yield break;
+        for (int i = 0; i < available.Count - 1; i++)
+        {
+            pos = available[i];
+            tmp = GameManager.Instance.Map[pos.X, pos.Y].CharacterOnTile;
+            GameManager.Instance.StartCoroutine(caster.HitAttack(tmp, GetDamage()));
+        }
+        pos = available[available.Count - 1];
+        tmp = GameManager.Instance.Map[pos.X, pos.Y].CharacterOnTile;
+        yield return GameManager.Instance.StartCoroutine(caster.HitAttack(tmp, GetDamage()));
+    }
+    public void CardRoutineInterrupt()
+    {
+        interrupted = true;
+    }
+    public int GetCost()
+    {
+        return 3;
+    }
+    public CostType GetCostType()
+    {
+        return CostType.PaladinEnergy;
+    }
+    public CardType GetCardType()
+    {
+        return CardType.Attack;
+    }
+    public int GetCardID()
+    {
+        return 1103010;
+    }
+}
