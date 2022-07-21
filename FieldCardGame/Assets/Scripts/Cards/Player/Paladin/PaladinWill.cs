7fc8c676d27dcf82ee8bf5308b69e566aef132b4
RYZEN-PC\RYZEN <jjun0403@korea.ac.kr>
2022-07-22 06:16:28 +0900

    전진, 녹슬지 않는 의지 구현

---
+++ FieldCardGame/Assets/Scripts/Cards/Player/Paladin/PaladinWill.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaladinWill : IPlayerCard
+{
+    private int range = 0;
+    private bool interrupted;
+    private int amount = 0;
+    private int cost = 1;
+    public bool Disposable { get; set; }
+
+    public int GetRange()
+    {
+        return range;
+    }
+    public void SetRange(int _range)
+    {
+        range = _range;
+    }
+    public int GetAmount()
+    {
+        return amount;
+    }
+    public void SetAmount(int _amount)
+    {
+        amount = _amount;
+    }
+    public Color GetUnAvailableTileColor()
+    {
+        return Color.red;
+    }
+    public List<Coordinate> GetAvailableTile(Coordinate pos)
+    {
+        List<Coordinate> ret = new List<Coordinate>();
+        ret.Add(pos);
+        return ret;
+    }
+    public Color GetAvailableTileColor()
+    {
+        return Color.blue;
+    }
+
+    public List<Coordinate> GetAreaofEffect(Coordinate relativePos)
+    {
+        List<Coordinate> ret = new List<Coordinate>();
+        ret.Add(new Coordinate(0, 0));
+        return ret;
+    }
+
+    public Color GetColorOfEffect(Coordinate pos)
+    {
+        if (pos.X == 0 && pos.Y == 0)
+        {
+            return Color.white;
+        }
+        return Color.black;
+    }
+
+    public bool IsAvailablePosition(Coordinate caster, Coordinate target)
+    {
+        List<Coordinate> availablePositions = GetAvailableTile(caster);
+        if (availablePositions.Exists((i) => i.X == target.X && i.Y == target.Y))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public IEnumerator CardRoutine(Character caster, Coordinate target)
+    {
+        if (interrupted)
+        {
+            interrupted = false;
+            yield break;
+        }
+        caster.EffectHandler.BuffDict[BuffType.Will].SetEffect(GetAmount());
+        yield break;
+    }
+    public void CardRoutineInterrupt()
+    {
+        interrupted = true;
+    }
+
+    public int GetCost()
+    {
+        return cost;
+    }
+
+    public CostType GetCostType()
+    {
+        return CostType.PaladinEnergy;
+    }
+
+    public CardType GetCardType()
+    {
+        return CardType.Skill;
+    }
+
+    public int GetCardID()
+    {
+        return 1216001;
+    }
+}
