02e6aa1d0b4da94b354a0a068b55153d651084b7
milk-way <joming1418@gmail.com>
2022-07-25 01:45:00 +0900

    WarlockGetSoul 구현

---
+++ FieldCardGame/Assets/Scripts/Cards/Player/Warlock/WarlcokGetSoul.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarlockGetSoul : IPlayerCard
+{
+    public bool Disposable { get; set; }
+    private int range = 0;
+    private int cost = 12;
+    private int giveCard = 3;
+    private bool interrupted;
+    public int GetRange()
+    {
+        return range;
+    }
+    public void SetRange(int _range)
+    {
+        range = _range;
+    }
+    public Color GetUnAvailableTileColor()
+    {
+        return Color.red;
+    }
+    public List<Coordinate> GetAvailableTile(Coordinate pos)
+    {
+        List<Coordinate> ret = new List<Coordinate>();
+        ret.Add(pos);
+        return ret;
+    }
+    public Color GetAvailableTileColor()
+    {
+        return Color.blue;
+    }
+    public List<Coordinate> GetAreaofEffect(Coordinate relativePos)
+    {
+        List<Coordinate> ret = new List<Coordinate>();
+        ret.Add(new Coordinate(0, 0));
+        return ret;
+    }
+    public Color GetColorOfEffect(Coordinate pos)
+    {
+        if (pos.X == 0 && pos.Y == 0)
+        {
+            return Color.white;
+        }
+        return Color.black;
+    }
+    public bool IsAvailablePosition(Coordinate caster, Coordinate target)
+    {
+        List<Coordinate> availablePositions = GetAvailableTile(caster);
+        if (availablePositions.Exists((i) => i.X == target.X && i.Y == target.Y))
+        {
+            return true;
+        }
+        return false;
+    }
+    public IEnumerator CardRoutine(Character caster, Coordinate target)
+    {
+        for (int i = 0; i < giveCard; i++)
+        {
+            yield return GameManager.Instance.StartCoroutine(caster.AddCard(GameManager.Instance.CardDict[3026001],true));
+        }
+        yield return null;
+    }
+    public void CardRoutineInterrupt()
+    {
+        interrupted = true;
+    }
+    public int GetCost()
+    {
+        return cost;
+    }
+    public void SetCost(int _cost)
+    {
+        cost = _cost;
+    }
+    public CostType GetCostType()
+    {
+        return CostType.Hp;
+    }
+    public CardType GetCardType()
+    {
+        return CardType.Skill;
+    }
+    public int GetCardID()
+    {
+        return 3125001;
+    }
+
+}
