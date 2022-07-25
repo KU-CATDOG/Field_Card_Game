44cfd14d0e2df12e0d829f1e1c24eb06147431f0
DESKTOP-NE7C9EQ\WooSeok <www013181@gmail.com>
2022-07-26 03:08:13 +0900

    자폭병 (Suicide Bomber) 구현

--- FieldCardGame/Assets/Scripts/Managers/GameManager.cs
+++ FieldCardGame/Assets/Scripts/Managers/GameManager.cs
@@ -231,7 +231,7 @@ public class GameManager : MonoBehaviour
         CharacterSelected.position = new Coordinate(10, 10);
         Map[10, 10].CharacterOnTile = CharacterSelected;
         CharacterSelected.SightUpdate(CharacterSelected.Sight);
-        Character enemy = Instantiate(EnemyDict[4]);
+        Character enemy = Instantiate(EnemyDict[5]);
         enemy.position = new Coordinate(15, 15);
         Character enemy2 = Instantiate(EnemyDict[2]);
         enemy2.position = new Coordinate(30, 30);
