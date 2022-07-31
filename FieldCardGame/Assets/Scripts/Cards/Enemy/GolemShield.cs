ddc7722852de92a5624470f019fc568373f698e8
DESKTOP-KHL63IV\jjun0403 <jjun0403@korea.ac.kr>
2022-07-31 14:05:29 +0900

    성기사 불타는 떨기나무, GolemShield 구현
    (테스트 아직 해보지 못 함)

---
+++ FieldCardGame/Assets/Scripts/Cards/Enemy/GolemShield.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolemShield : ICard
+{
+    public bool Disposable { get; set; }
+
+    private int cost = 1;
+    private int amount = 15;
+    private int range = 0;
+
+    private bool interrupted;
+
+    public IEnumerator GetCardRoutine(Character owner)
+    {
+        yield break;
+    }
+    public IEnumerator RemoveCardRoutine(Character owner)
+    {
+        yield break;
+    }
+
+    public int GetCardID()
+    {
+        return 0002123;
+    }
+
+    public CostType GetCostType()
+    {
+        return CostType.MonsterCrystal;
+    }
+    public CardType GetCardType()
+    {
+        return CardType.Skill;
+    }
+
+    public List<Coordinate> GetAvailableTile(Coordinate pos)
+    {
+        List<Coordinate> ret = new List<Coordinate>();
+        ret.Add(pos);
+        return ret;
+
+    }
+    public bool IsAvailablePosition(Coordinate caster, Coordinate target)
+    {
+        List<Coordinate> availablePositions = GetAvailableTile(caster);
+        if (availablePositions.Exists((i) => i.X == target.X && i.Y == target.Y))
+        {
+            return true;
+        }
+        return false;
+    }
+    public List<Coordinate> GetAreaofEffect(Coordinate relativePos)
+    {
+        List<Coordinate> ret = new List<Coordinate>();
+        ret.Add(new Coordinate(0, 0));
+        return ret;
+    }
+    public IEnumerator CardRoutine(Character caster, Coordinate center)
+    {
+        if (interrupted)
+        {
+            interrupted = false;
+            yield break;
+        }
+        caster.EffectHandler.BuffDict[BuffType.Shield].SetEffect(GetAmount());
+        yield break;
+    }
+    public void CardRoutineInterrupt()
+    {
+        interrupted = true;
+    }
+    public int GetAmount()
+    {
+        return amount;
+    }
+    public void SetAmount(int _amount)
+    {
+        range = _amount;
+    }
+
+
+    
+    public int GetRange()
+    {
+        return range;
+    }
+    public void SetRange(int _range)
+    {
+        range = _range;
+    }
+    public int GetCost()
+    {
+        return cost;
+    }
+    public void SetCost(int _cost)
+    {
+        cost = _cost;
+    }
+}
