7ab429708e81b388e2147af85383fb82d2a93ec4
duru100470 <zenpia@naver.com>
2022-08-03 22:14:07 +0900

    팔라딘 정화 구현

---
+++ FieldCardGame/Assets/Scripts/Cards/Player/Paladin/PaladinPurify.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaladinPurify : IPlayerCard
+{
+    public bool Disposable { get; set; }
+    private int range = 0;
+    private int cost = 2;
+    private int amount = 0;
+    private bool interrupted;
+    public string ExplainText
+    {
+        get
+        {
+            return $"적용된 모든 디버프를 해제합니다.";
+        }
+    }
+    public IEnumerator GetCardRoutine(Character owner)
+    {
+        yield break;
+    }
+    public IEnumerator RemoveCardRoutine(Character owner)
+    {
+        yield break;
+    }
+    public int GetRange()
+    {
+        return range;
+    }
+    public void SetRange(int _range)
+    {
+        range = _range;
+    }
+    public int GetAmount()
+    {
+        return amount;
+    }
+    public void SetAmount(int _amount)
+    {
+        amount = _amount;
+    }
+    public Color GetUnAvailableTileColor()
+    {
+        return Color.red;
+    }
+    public List<Coordinate> GetAvailableTile(Coordinate pos)
+    {
+        List<Coordinate> ret = new List<Coordinate>();
+        ret.Add(pos);
+        return ret;
+    }
+    public Color GetAvailableTileColor()
+    {
+        return Color.blue;
+    }
+    public List<Coordinate> GetAreaofEffect(Coordinate relativePos)
+    {
+        List<Coordinate> ret = new List<Coordinate>();
+        ret.Add(new Coordinate(0, 0));
+        return ret;
+    }
+    public Color GetColorOfEffect(Coordinate pos)
+    {
+        if (pos.X == 0 && pos.Y == 0)
+        {
+            return Color.white;
+        }
+        return Color.black;
+    }
+    public bool IsAvailablePosition(Coordinate caster, Coordinate target)
+    {
+        List<Coordinate> availablePositions = GetAvailableTile(caster);
+        if (availablePositions.Exists((i) => i.X == target.X && i.Y == target.Y))
+        {
+            return true;
+        }
+        return false;
+    }
+    public IEnumerator CardRoutine(Character caster, Coordinate target)
+    {
+        if (interrupted)
+        {
+             interrupted = false;
+             yield break;
+        }
+        foreach (var debuff in caster.EffectHandler.DebuffDict.Values)
+        {
+            if (debuff.IsEnabled)
+                debuff.ForceRemoveEffect();
+        }
+        yield break;
+    }
+    public void CardRoutineInterrupt()
+    {
+        interrupted = true;
+    }
+    public int GetCost()
+    {
+        return cost;
+    }
+    public void SetCost(int _cost)
+    {
+        cost = _cost;
+    }
+    public CostType GetCostType()
+    {
+        return CostType.PaladinEnergy;
+    }
+    public CardType GetCardType()
+    {
+        return CardType.Skill;
+    }
+    public int GetCardID()
+    {
+        return 1127001;
+    }
+}
