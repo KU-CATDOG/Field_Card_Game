57d16b4246c57a78fdb32568f93c442c8dbc7222
milk-way <joming1418@gmail.com>
2022-08-04 19:55:13 +0900

    WarlockAntiBuff 구현(확인 안해봄)

---
+++ FieldCardGame/Assets/Scripts/Cards/Player/Warlock/WarlockAntiBuff.cs
@@ -0,0 +1,155 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarlockAntiBuff : IPlayerCard
+{
+    public bool Disposable { get; set; }
+    private int range = 2;
+    private int cost = 15;
+    private bool interrupted;
+    public string ExplainText
+    {
+        get
+        {
+            return $"대상에게 걸려있는 버프를 모두 제거합니다.";
+        }
+    }
+    public IEnumerator GetCardRoutine(Character owner)
+    {
+        yield break;
+    }
+    public IEnumerator RemoveCardRoutine(Character owner)
+    {
+        yield break;
+    }
+    public int GetRange()
+    {
+        return range;
+    }
+    public void SetRange(int _range)
+    {
+        range = _range;
+    }
+    public Color GetUnAvailableTileColor()
+    {
+        return Color.red;
+    }
+    public List<Coordinate> GetAvailableTile(Coordinate pos)
+    {
+        List<Coordinate> ret = new List<Coordinate>();
+        int level = 1;
+        bool[,] visited = new bool[128, 128];
+        Queue<Coordinate> queue = new Queue<Coordinate>();
+        Queue<Coordinate> nextQueue = new Queue<Coordinate>();
+        queue.Enqueue(pos);
+        while (level++ <= GetRange())
+        {
+            while (queue.Count != 0)
+            {
+                Coordinate tmp = queue.Dequeue();
+                if (tmp.X != pos.X || tmp.Y != pos.Y)
+                    ret.Add(tmp);
+                Coordinate tile;
+                if ((tile = tmp.GetDownTile()) != null && !visited[tile.X, tile.Y])
+                {
+                    visited[tile.X, tile.Y] = true;
+                    nextQueue.Enqueue(tile);
+                };
+                if ((tile = tmp.GetLeftTile()) != null && !visited[tile.X, tile.Y])
+                {
+                    visited[tile.X, tile.Y] = true;
+                    nextQueue.Enqueue(tile);
+                };
+                if ((tile = tmp.GetRightTile()) != null && !visited[tile.X, tile.Y])
+                {
+                    visited[tile.X, tile.Y] = true;
+                    nextQueue.Enqueue(tile);
+                };
+                if ((tile = tmp.GetUpTile()) != null && !visited[tile.X, tile.Y])
+                {
+                    visited[tile.X, tile.Y] = true;
+                    nextQueue.Enqueue(tile);
+                }
+            }
+            queue = new Queue<Coordinate>(nextQueue);
+            nextQueue.Clear();
+        }
+        while (queue.Count != 0)
+        {
+            Coordinate tmp = queue.Dequeue();
+            if (tmp.X != pos.X || tmp.Y != pos.Y)
+                ret.Add(tmp);
+        }
+        return ret;
+    }
+    public Color GetAvailableTileColor()
+    {
+        return Color.blue;
+    }
+    public List<Coordinate> GetAreaofEffect(Coordinate relativePos)
+    {
+        List<Coordinate> ret = new List<Coordinate>();
+        ret.Add(new Coordinate(0, 0));
+        return ret;
+    }
+    public Color GetColorOfEffect(Coordinate pos)
+    {
+        if (pos.X == 0 && pos.Y == 0)
+        {
+            return Color.white;
+        }
+        return Color.black;
+    }
+    public bool IsAvailablePosition(Coordinate caster, Coordinate target)
+    {
+        List<Coordinate> availablePositions = GetAvailableTile(caster);
+        if (availablePositions.Exists((i) => i.X == target.X && i.Y == target.Y))
+        {
+            return true;
+        }
+        return false;
+    }
+    public IEnumerator CardRoutine(Character caster, Coordinate target)
+    {
+        if (interrupted)
+        {
+             interrupted = false;
+             yield break;
+        }
+        Character tmp = GameManager.Instance.Map[target.X, target.Y].CharacterOnTile;
+        if (tmp)
+        {
+            foreach (var i in tmp.EffectHandler.BuffDict)
+            {
+                i.Value.ForceRemoveEffect();
+            }
+        }
+        yield break;
+    }
+    public void CardRoutineInterrupt()
+    {
+        interrupted = true;
+    }
+    public int GetCost()
+    {
+        return cost;
+    }
+    public void SetCost(int _cost)
+    {
+        cost = _cost;
+    }
+    public CostType GetCostType()
+    {
+        return CostType.Hp;
+    }
+    public CardType GetCardType()
+    {
+        return CardType.Skill;
+    }
+    public int GetCardID()
+    {
+        return 3043001;
+    }
+
+}
