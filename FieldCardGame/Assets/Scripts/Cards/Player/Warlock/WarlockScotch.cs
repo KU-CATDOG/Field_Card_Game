ac00e96294a4787b4d8751ff50e45744d3e2a705
milk-way <joming1418@gmail.com>
2022-08-16 19:08:43 +0900

    흑마법사 숨통끊기 카드 구현

---
+++ FieldCardGame/Assets/Scripts/Cards/Player/Warlock/WarlockScotch.cs
@@ -0,0 +1,165 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarlockScotch : IPlayerCard
+{
+    public bool Disposable { get; set; }
+    private int range = 2;
+    private int cost = 15;
+    private int amount = 5;
+    private bool interrupted;
+    public string ExplainText
+    {
+        get
+        {
+            return $"이번 턴에 사용한 공격카드 한 장 당 5의 피해를 줍니다.";
+        }
+    }
+    public IEnumerator GetCardRoutine(Character owner)
+    {
+        yield break;
+    }
+    public IEnumerator RemoveCardRoutine(Character owner)
+    {
+        yield break;
+    }
+    public int GetRange()
+    {
+        return range;
+    }
+    public void SetRange(int _range)
+    {
+        range = _range;
+    }
+    public int GetAmount()
+    {
+        return amount;
+    }
+    public void SetAmount(int _amount)
+    {
+        amount = _amount;
+    }
+    public Color GetUnAvailableTileColor()
+    {
+        return Color.red;
+    }
+    public List<Coordinate> GetAvailableTile(Coordinate pos)
+    {
+        List<Coordinate> ret = new List<Coordinate>();
+        int level = 1;
+        bool[,] visited = new bool[128, 128];
+        Queue<Coordinate> queue = new Queue<Coordinate>();
+        Queue<Coordinate> nextQueue = new Queue<Coordinate>();
+        queue.Enqueue(pos);
+        while (level++ <= GetRange())
+        {
+            while (queue.Count != 0)
+            {
+                Coordinate tmp = queue.Dequeue();
+                if(tmp.X != pos.X || tmp.Y != pos.Y)
+                    ret.Add(tmp);
+                Coordinate tile;
+                if ((tile = tmp.GetDownTile()) != null && !visited[tile.X, tile.Y])
+                {
+                    visited[tile.X, tile.Y] = true;
+                    nextQueue.Enqueue(tile);
+                };
+                if ((tile = tmp.GetLeftTile()) != null && !visited[tile.X, tile.Y])
+                {
+                    visited[tile.X, tile.Y] = true;
+                    nextQueue.Enqueue(tile);
+                };
+                if ((tile = tmp.GetRightTile()) != null && !visited[tile.X, tile.Y])
+                {
+                    visited[tile.X, tile.Y] = true;
+                    nextQueue.Enqueue(tile);
+                };
+                if ((tile = tmp.GetUpTile()) != null && !visited[tile.X, tile.Y])
+                {
+                    visited[tile.X, tile.Y] = true;
+                    nextQueue.Enqueue(tile);
+                }
+            }
+            queue = new Queue<Coordinate>(nextQueue);
+            nextQueue.Clear();
+        }
+        while (queue.Count != 0)
+        {
+            Coordinate tmp = queue.Dequeue();
+            if (tmp.X != pos.X || tmp.Y != pos.Y)
+                ret.Add(tmp);
+        }
+        return ret;
+    }
+    public Color GetAvailableTileColor()
+    {
+        return Color.blue;
+    }
+    public List<Coordinate> GetAreaofEffect(Coordinate relativePos)
+    {
+        List<Coordinate> ret = new List<Coordinate>();
+        ret.Add(new Coordinate(0, 0));
+        return ret;
+    }
+    public Color GetColorOfEffect(Coordinate pos)
+    {
+        if (pos.X == 0 && pos.Y == 0)
+        {
+            return Color.white;
+        }
+        return Color.black;
+    }
+    public bool IsAvailablePosition(Coordinate caster, Coordinate target)
+    {
+        List<Coordinate> availablePositions = GetAvailableTile(caster);
+        if (availablePositions.Exists((i) => i.X == target.X && i.Y == target.Y))
+        {
+            return true;
+        }
+        return false;
+    }
+    public IEnumerator CardRoutine(Character caster, Coordinate target)
+    {
+        if (interrupted)
+        {
+             interrupted = false;
+             yield break;
+        }
+        int attackCount = caster.attackCardUseInTurn;
+        Character tmp = GameManager.Instance.Map[target.X, target.Y].CharacterOnTile;
+        if (tmp)
+        {
+            for (int i = 0; i < attackCount - 1; i++)
+            {
+                yield return GameManager.Instance.StartCoroutine(caster.HitAttack(tmp, GetAmount()));
+            }
+        }
+        yield break;
+    }
+    public void CardRoutineInterrupt()
+    {
+        interrupted = true;
+    }
+    public int GetCost()
+    {
+        return cost;
+    }
+    public void SetCost(int _cost)
+    {
+        cost = _cost;
+    }
+    public CostType GetCostType()
+    {
+        return CostType.Hp;
+    }
+    public CardType GetCardType()
+    {
+        return CardType.Attack;
+    }
+    public int GetCardID()
+    {
+        return 3029010;
+    }
+
+}
