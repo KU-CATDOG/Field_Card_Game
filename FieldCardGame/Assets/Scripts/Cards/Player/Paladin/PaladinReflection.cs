06ead7663c5684ec51b2a3b1fcb10bbd009766bb
RYZEN-PC\RYZEN <jjun0403@korea.ac.kr>
2022-08-19 03:29:47 +0900

    성찰, 순발력 작업중

---
+++ FieldCardGame/Assets/Scripts/Cards/Player/Paladin/PaladinReflection.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaladinReflection : IPlayerCard
+{
+    private int range = 0;
+    private bool interrupted;
+    private int cost = 0;
+    public bool Disposable { get; set; }
+    public string ExplainText
+    {
+        get
+        {
+            return $"버린 카드 더미에서 카드를 하나 선택하여 손패로 가져옵니다.";
+        }
+    }
+    public IEnumerator GetCardRoutine(Character owner)
+    {
+        yield break;
+    }
+    public IEnumerator RemoveCardRoutine(Character owner)
+    {
+        yield break;
+    }
+    public int GetRange()
+    {
+        return range;
+    }
+    public void SetRange(int _range)
+    {
+        range = _range;
+    }
+    public Color GetUnAvailableTileColor()
+    {
+        return Color.red;
+    }
+
+    public List<Coordinate> GetAvailableTile(Coordinate pos)
+    {
+        List<Coordinate> ret = new List<Coordinate>();
+        ret.Add(pos);
+        return ret;
+    }
+
+    public Color GetAvailableTileColor()
+    {
+        return Color.blue;
+    }
+
+    public List<Coordinate> GetAreaofEffect(Coordinate relativePos)
+    {
+        List<Coordinate> ret = new List<Coordinate>();
+        ret.Add(new Coordinate(0, 0));
+        return ret;
+    }
+
+    public Color GetColorOfEffect(Coordinate pos)
+    {
+        if (pos.X == 0 && pos.Y == 0)
+        {
+            return Color.white;
+        }
+        return Color.black;
+    }
+
+    public bool IsAvailablePosition(Coordinate caster, Coordinate target)
+    {
+        List<Coordinate> availablePositions = GetAvailableTile(caster);
+        if (availablePositions.Exists((i) => i.X == target.X && i.Y == target.Y))
+        {
+            return true;
+        }
+        return false;
+    }
+    public IEnumerator CardRoutine(Character caster, Coordinate target)
+    {
+        if (interrupted)
+        {
+            interrupted = false;
+            yield break;
+        }
+        PlayerUIManager.Instance.OpenDiscardedPilePanel();
+    }
+    public void CardRoutineInterrupt()
+    {
+        interrupted = true;
+    }
+    public int GetCost()
+    {
+        return cost;
+    }
+    public void SetCost(int value)
+    {
+        cost = value;
+    }
+    public CostType GetCostType()
+    {
+        return CostType.PaladinEnergy;
+    }
+    public CardType GetCardType()
+    {
+        return CardType.Skill;
+    }
+    public int GetCardID()
+    {
+        return 1129001;
+    }
+}
+
